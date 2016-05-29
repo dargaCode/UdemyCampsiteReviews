e583f708939104cbe151d72c40ec7ebc4db46d42
dargaCode <darga.code@gmail.com>
2016-05-28 19:54:33 -0700

    Display comments in the Show campsite view
    - Populate the campsite's comments array with actual comments
      (rather than just their itemIds) before rendering show.ejs
    - Add a loop in show.ejs to display each comment and author

--- app.js
+++ app.js
@@ -76,14 +76,17 @@ app.post('/campsites', function(req, res) {
 app.get('/campsites/:id', function(req, res) {
   const id = req.params.id;
 
-  Campsite.findById(id, function(err, foundCampsite) {
-    if (err) {
-      console.log('ERROR:', err);
-      res.redirect('/campsites/');
-    } else {
-      res.render('show', {site: foundCampsite});
-    }
-  });
+  Campsite
+    .findById(id)
+    .populate('comments')
+    .exec(function(err, foundCampsite) {
+      if (err) {
+        console.log('ERROR:', err);
+        res.redirect('/campsites/');
+      } else {
+        res.render('show', {site: foundCampsite});
+      }
+    });
 });
 
   //Route - Edit
